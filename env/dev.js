a316c9d7eadf3e95c7f988e6c3e825e53d963f46
Popper.Li <519318459@qq.com>
2020-06-01 14:57:31 +0800

    update
    新增程序基础/环境配置
    新增scss目录

---
+++ env/dev.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: 开发环境配置
+ * @Date: 2020-06-01 13:36:29
+ * @Autor: Popper_Li
+ * @LastEditors: Popper_Li
+ * @LastEditTime: 2020-06-01 13:52:04
+ */ 
+
+module.exports = {
+  // 项目路径
+  pulicPath: '',
+  // 默认请求路径
+  apiBase: 'https://xxx.xxx.com/api/'
+}
---
+++ env/prod.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: 正式环境配置
+ * @Date: 2020-06-01 13:37:08
+ * @Autor: Popper_Li
+ * @LastEditors: Popper_Li
+ * @LastEditTime: 2020-06-01 13:53:18
+ */ 
+module.exports = {
+  // 项目路径
+  pulicPath: '',
+  // 默认请求路径
+  apiBase: 'https://xxx.xxx.com/api/'
+}
